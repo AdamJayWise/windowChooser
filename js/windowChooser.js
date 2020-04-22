22962e3c694054689e4bfa2ddbc4bef3e0ff0749
AdamWise <wise.adam.jay@gmail.com>
2020-04-22 09:09:17 -0400

    more misc

--- js/windowChooser.js
+++ js/windowChooser.js
@@ -10,7 +10,8 @@ console.log("windowChooser - (C) Adam Wise 2020")
  * by default show some sCMOS camera
  * 
  * 
- * ok what now - 
+ * ok what now - adding curves.  When the product changes, all the paths in the canvas should be removed
+ * and a path appended to the canvas for each available window, with only the default visible at first
  */
 
 // json structure of the product families
@@ -23,7 +24,7 @@ var families = {
         {
             'displayName' : 'Marana 4.2BV-11',
             'mechanicalSpecification' : 'WN50FS',
-            'defaultWindow' : '(BB-VS-NR)U',
+            'defaultWindow' : '(BB-VS-NR)U' ,
             'availableWindows' : ['(BB-VV-NR)U', '(VS-NR-ENH)W']
         },
 
@@ -66,15 +67,32 @@ var families = {
     }
 }
 
-var windowDict = {
-    '(BB-VS-NR)U' : 'Broadband VIS-NIR Unwedged',
-    '(BB-VS-NR)W' : 'Broadband VIS-NIR Unwedged',
-    '(BB-VV-NR)U' : 'Broadband VUV-NIR Unwedged',
-    '(VS-NR-ENH)W' : 'VIS-NIR Enhanced Wedged'
+// add window + camera => transmission graph lookup table here  
+optLUT = {
+    // marana 4VB6
+    '(BB-VS-NR)U Marana-4VB6' : 'OPT-14344',
+    // marana 4BV-11
+    '(BB-VS-NR)U Marana 4BV-11' : 'Marana11(BB-VS-NR)',
+    '(BB-VV-NR)U Marana 4BV-11' : '(BB-VV-NR)',
+    '(VS-NR-ENH)W Marana 4BV-11' : 'OPT-15718',
+    //marana 4BU-11
+    '(BB-VV-NR)U Marana-4BU11' : '(BB-VV-NR)',
+    // Sona 4/2BV11
+    '(BB-VS-NR)U Sona-2BV11' : 'Marana11(BB-VS-NR)',
+    '(BB-VS-NR)W Sona-2BV11' : 'Marana11(BB-VS-NR)',
+    // sona 4.2B6
+    '(BB-VS-NR)U Sona 4.2B-6' : 'OPT-14344',
+    
+    
+
 }
 
-
-
+var windowDict = {
+    '(BB-VS-NR)U' : 'Broadband VIS-NIR, Unwedged',
+    '(BB-VS-NR)W' : 'Broadband VIS-NIR, Unwedged',
+    '(BB-VV-NR)U' : 'Broadband VUV-NIR, Unwedged',
+    '(VS-NR-ENH)W' : 'VIS-NIR Enhanced, Wedged'
+}
 
 
  function View(paramObj){
@@ -85,6 +103,63 @@ var windowDict = {
     self.family = ''; // this is the current product family, e.g. Sona of the view
     self.product = ''; // this is the specific product of the view
     self.productObj = {}; // this is the json object with the rest of the prouct config up in there
+    self.canvasWidth = 600; // chart canvas width in pixels
+    self.canvasHeight = 300; // chart canvas height in pixels
+    self.canvasMargin = 50; // svg margin in pixels
+    self.xTicks = [100, 500, 1100];
+    self.yTicks = [50, 75, 100];
+
+    // add scales
+    // generate scales and axes including formatting
+    self.xScale = d3.scaleLinear()
+                    .domain([100,1200])
+                    .range([self.canvasMargin, self.canvasWidth-self.canvasMargin])
+                    .clamp(true)
+
+    self.yScale = d3.scaleLinear()
+                    .domain([50, 100])
+                    .range([self.canvasHeight-self.canvasMargin, self.canvasMargin/5])
+                    .clamp(true)
+
+    self.dataLine = d3.line()
+                    .x(d=>self.xScale(d.x))
+                    .y(d=>self.yScale(d.y))
+
+    self.xAxis = d3.axisBottom()
+                    .scale(self.xScale)
+                    .tickValues( self.xTicks )
+                    .tickFormat(d=>d);
+
+    self.yAxis = d3.axisLeft()
+                    .scale(self.yScale)
+                    .tickValues( self.yTicks )
+                    .tickFormat(d=>d);
+
+    // add SVG 
+    self.svg = self.div
+    .append('div')
+    .attr('id','chartDiv')
+    .append('div')
+    .append('svg')
+    .attr('width', self.canvasWidth)
+    .attr('height', self.canvasHeight)
+
+    // add axes
+    self.svg
+        .append('g')
+        .classed('axis',true)
+        .attr('id','yAxis')
+        .attr('transform',`translate(${self.xScale(self.xTicks[0])-2},0)`)
+        .call(self.yAxis)
+        .style('font-size',13)
+                
+    self.svg
+        .append('g')
+        .classed('axis',true)
+        .attr('id','xAxis')
+        .attr('transform',`translate(0,${self.yScale(self.yTicks[0])-2})`)
+        .call(self.xAxis)
+        .style('font-size',13)
 
     // object properties
     self.controlDiv = self.div
@@ -156,7 +231,7 @@ var windowDict = {
         var windDescr = windowDict[defWind];
         
         // update default window display
-        self.defaultWindowDisplay.text(defWind +  " - " + windDescr);
+        self.defaultWindowDisplay.text(defWind +  " : " + windDescr);
 
         // hide the additional windows button if there is only one option
         if(self.productObj['availableWindows'].length < 2){
@@ -167,8 +242,33 @@ var windowDict = {
         }
 
         // add a list of additional windows
+        self.advancedWindowDiv.classed('hidden', true);
         self.advancedWindowDiv.selectAll('div').remove();
-        self.advancedWindowDiv.selectAll('div').data(self.productObj['availableWindows']).enter().append('div').text(d=>d)
+        self.advancedWindowDiv.selectAll('div')
+            .data(self.productObj['availableWindows'])
+            .enter()
+            .append('div')
+            .text(d=>d + ' - ' + windowDict[d])
+
+
+        // remove old traces from graph
+        self.svg.selectAll('path').remove();
+
+        // update traces on graph
+        for (var i in self.productObj.availableWindows){
+            var window = self.productObj.availableWindows[i];
+            console.log(window, self.product)
+            var tag = optLUT[window + ' ' + self.product];
+            console.log(tag)
+            var dataObj = trans[tag]
+            
+            // add traces to graph
+            self.svg.append('path')
+                .attr('fill','none')
+                .attr('stroke', 'black')
+                .attr('d', self.dataLine(dataObj))
+                //.attr('stroke-dasharray', this.dashArray)
+        }
 
         }
 
@@ -185,7 +285,6 @@ var windowDict = {
         self.advancedWindowDiv.classed('hidden', !self.advancedWindowDiv.classed('hidden'))
 
     }
-    
 
  }
 
